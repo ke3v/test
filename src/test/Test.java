9e63a866bf9c3b40929b24d57b9ceb9d9f293608
Jack Stavinoha-373001321 <373001321@vusdnoc.vusd.k12.ca.us>
2017-10-27 10:39:26 -0700

    new Code

--- src/test/Test.java
+++ src/test/Test.java
@@ -3,6 +3,21 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+/*
+How to Add Code to Repository
+1: git add -A
+2: git commit -m "new Code"
+3: git push origin master
+
+
+How to Get Code from Repository
+1: git pull origin master
+
+How to Get Clone from Repository
+1: git clone LINK
+
+
+*/
 package test;
 
 /**
